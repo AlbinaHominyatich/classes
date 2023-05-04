f9e7d1929bcacc7abf1cc31724f284823dbcaeff
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-05-04 19:49:32 +0300

    методи класів

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ class Student:
     def __init__(self, height = 150): #конструктор класу
         self.height = height
         Student.count += 1
+    def breathing(self): #методи класів
+        return self.height - 10
 oleg = Student() #об'єкт, екземпляр класу
 print(oleg.height)
 masha = Student(height = 200)
 print(masha.height)
 print(Student.count)
+print(masha.breathing())
