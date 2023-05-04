25e4a4285682e6866f007f18df902c0e1d998e21
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-05-04 20:39:03 +0300

    трете завдання

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-#Класи
-class Student:
-    print("Hi!")
-    count = 0
-    def __init__(self, height = 150): #конструктор класу
-        self.height = height
-        Student.count += 1
-    def breathing(self): #методи класів
-        return self.height - 10
-oleg = Student() #об'єкт, екземпляр класу
-print(oleg.height)
-masha = Student(height = 200)
-print(masha.height)
-print(Student.count)
-print(masha.breathing())
+#3
+class Aminals:
+    pass
+class Dog(Aminals):
+    def sound(self):
+        print("Гав")
+bobik = Dog()
+bobik.sound()
